b703fff15cc13c334a5e925b0960df794e137bdb
B-isaiah <aliahbb03@gmail.com>
2026-01-17 02:00:39 +0100

    add health check endpoint to verify API status

--- Backend_P1/src/app.js
+++ Backend_P1/src/app.js
@@ -15,6 +15,14 @@ app.use('/api/v1/posts', postRouter);
 //Route declarations
 app.use('/api/v1/users', userRouter);
 
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "API is running",
+    version: "v1"
+  });
+});
+
+
 //example route: http://localhost:3000/api/v1/users/register
 
 
